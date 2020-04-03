fb6b2385715206156a5aef102b04bf24eb53617f
agnelnieves <agnelnieves@gmail.com>
2020-04-03 14:11:36 -0400

    Feat: clean pages

--- pages/about.vue
+++ pages/about.vue
@@ -1,29 +1,9 @@
 <template>
   <div class="container">
     <div>
-      <logo />
       <h1 class="title">
-        covidwatch
+        About
       </h1>
-      <h2 class="subtitle">
-        
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
     </div>
   </div>
 </template>
