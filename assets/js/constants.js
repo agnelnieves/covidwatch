59079c7be6ed4ba0ca834f11c9d7fd08bad085ac
Winkode <winkode.agency@gmail.com>
2020-04-04 01:59:15 -0400

    Feat: 3d Globe integration

---
+++ assets/js/constants.js
@@ -0,0 +1,12 @@
+export const GLOBE_IMAGE_URL =
+  '//raw.githubusercontent.com/agnelnieves/covidwatch/master/src/assets/earth-dark.jpg'
+export const GLOBE_BG_COLOR =
+  '//raw.githubusercontent.com/agnelnieves/covidwatch/master/src/assets/image%2013.png'
+// export const GLOBE_IMAGE_URL = '//unpkg.com/three-globe@2.6.9/example/img/earth-dark.jpg';
+export const BACKGROUND_IMAGE_URL =
+  '//raw.githubusercontent.com/agnelnieves/covidwatch/master/src/assets/night-sky.png'
+export const BACKGROUND_COLOR = '#172B4D'
+export const BUMP_IMAGE = 'unpkg.com/three-globe/example/img/earth-topology.png'
+export const GEOJSON_URL =
+  'https://raw.githubusercontent.com/nvkelso/natural-earth-vector/master/geojson/ne_110m_admin_0_countries.geojson'
+export const CASES_API = 'https://corona.lmao.ninja/countries?sort=country'
